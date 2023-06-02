a57ab3ce3db57b75fb563d5ef241bebff2dc2444
Pierson Marks <piersonmarks@engineering.ucla.edu>
2023-06-01 22:17:46 -0700

    Fix footer on small devices

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
           </div>
           <div className="text-gray-700 mt-4"> 
             <div className='text-justify pb-2 text-xl font-bold'>Media & Press</div>
-            <ul className="align-middle list-inside">
+            <ul className="align-middle list-outside ml-4">
               <li className='list-disc align-middle'>
                 <a href="https://www.sonos.com/en-us/newsroom/sonos-and-alexa-expand" target="_blank" className="hover:bg-blue-200 hover:underline hover:transition-all duration-150 ease-in">Sonos and Amazon to expand access to voice control with Alexa on Sonos devices</a>
               </li>
@@ -29,8 +29,8 @@ function App() {
           </div>
 
         </main>
-        <div className="fixed sticky w-full h-16 ">
-            <div className="grid text-md text-gray-500 justify-center mx-auto mb-3">
+        <div className="fixed sticky w-full">
+            <div className="grid text-md text-gray-500 justify-center text-center mx-auto mb-3">
               <span>Want to chat? Contact me at <span className="italic">piersonmarks@engineering.ucla.edu</span></span>
             </div>
             <div className="grid text-sm text-gray-500 justify-center mx-auto">
@@ -44,7 +44,6 @@ function App() {
                 <svg className="fill-gray-500 hover:fill-blue-800" width="18" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>LinkedIn</title><path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/></svg>
               </a>
             </div>
-          
         </div>
       </div>
     </div>
